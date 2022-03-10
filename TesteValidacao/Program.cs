07c4c399c4721228856382f888e213414e1cb2d8
Matheus Mello <isadora.minuzzi@gmail.com>
2022-03-10 15:34:26 -0300

    primeiro commit

---
+++ TesteValidacao/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TesteValidacao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
